920f894c2877a139667e93fb3e286fa74c9f9ad5
Ernesto <ernestorod27@gmail.com>
2021-05-28 12:27:39 -0400

    Initial commit for backend

---
+++ NewtechTestAPI/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtechTestAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Role { get; set; }
+
+    }
+}
---
+++ NewtechTestAPI/Models/UsersDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtechTestAPI.Models
+{
+    public class UsersDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+
+        public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+        }
+
+    }
+}
